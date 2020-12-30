0b26cc5e1edf00d9fe2fd705a5cf1d7d334177e7
itsnotreal675 <76721666+itsnotreal675@users.noreply.github.com>
2020-12-30 03:03:47 -0300

    Add files via upload

---
+++ src/help.js
@@ -0,0 +1,25 @@
+const help = () => {
+	return '> ```HDB BOTv4.1 Segue os comandos``` <\n' +
+	'comando : *!sticker* ou *!stiker*\n' +
+	'descrição : converta imagem/gif/video para figurinha\n' +
+	'como usar : marque a imagem/gif/video, ou envie a imagem/gif/video com a legenda descrita acima\n\n' + 
+	'comando : *!toimg*\n' +
+	'descrição : converta figurinhas para imagens\n' +
+	'como usar : marque a figurinha com a legenda descrita acima  Atenção não utilizar este comando em figurinhas animadas!\n\n' +
+	'comando : *!tsticker* ou *!tstiker*\n' +
+	'descrição : converta texto para figurinha\n' +
+	'como usar : *!tsticker seu texto aqui*\n\n' +
+	'> ```Meme Comandos``` <\n' +
+	'comando : *!meme*\n' +
+	'descrição : memes gerados aleatórios  [english]\n' +
+	'como usar: marque a imagem com comando relatado acima\n\n' +
+	'comando : *!memeindo*\n' +
+	'descrição : memes aleatorios gringos [indo]\n' +
+	'como usar : basta enviar o comando\n\n' +
+	'> ```Comando Texto para Fala``` <\n' +
+	'comando : *!gtts*\n' +
+	'descrição : converta texto para/audio\n' +
+	'como usar : *!gtts [cc] [texto]*\nexemplo : *!gtts pt Oi amor tá podendo falar ou sua mulher tá perto*'
+}
+
+exports.help = help
